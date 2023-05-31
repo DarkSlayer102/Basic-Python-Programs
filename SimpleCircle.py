660b0a1150568f5af93dc748bf9d04e2794242e0
DarkSlayer <55598746+DarkSlayer102@users.noreply.github.com>
2023-05-31 14:56:50 +0100

    Create SimpleCircle.py
    Simple Turtle Circle

---
+++ SimpleCircle.py
@@ -0,0 +1,24 @@
+import turtle
+
+
+def main():
+
+    my_turtle = turtle.Turtle()
+
+
+    my_turtle.speed(2)
+
+
+
+    def draw_circle():
+        my_turtle.circle(100)
+
+
+
+    draw_circle()
+
+    turtle.done()
+
+
+if __name__ == '__main__':
+    main()
